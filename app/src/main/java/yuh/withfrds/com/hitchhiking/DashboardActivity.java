0a5fa35cff33121d65da812648b8871b3a0bd941
MichaelYutaoWang <MichaelYutaoWang@users.noreply.github.com>
2018-06-19 10:51:17 +1200

    icon
    change icon to new picture

---
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/DashboardActivity.java
@@ -0,0 +1,17 @@
+package yuh.withfrds.com.hitchhiking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DashboardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        // ref: https://www.dev2qa.com/android-listview-example/
+        // ref: https://alvinalexander.com/source-code/android/android-listactivity-and-listview-example
+    }
+
+}
