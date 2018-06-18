4a3d98d9bcb628fbd41ae18069d2fb841b342f55
MichaelYutaoWang <MichaelYutaoWang@users.noreply.github.com>
2018-06-18 21:44:35 +1200

    rename
    change RequestActivity to PostActivity

--- app/src/main/java/yuh/withfrds/com/hitchhiking/PostActivity.java
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/PostActivity.java
@@ -3,11 +3,11 @@ package yuh.withfrds.com.hitchhiking;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class RequestActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_request);
+        setContentView(R.layout.activity_post);
     }
 }
