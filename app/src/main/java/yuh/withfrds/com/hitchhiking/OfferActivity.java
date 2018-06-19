4fe95ea1b6f4741f3090927da6208c8fd4e1cecc
tim-hub <tim_w@outlook.com>
2018-06-20 11:20:03 +1200

    fix the UI of offer activity
    - get the button back
    - disable the default app bar to avoid the conflics with the side menu

--- app/src/main/java/yuh/withfrds/com/hitchhiking/OfferActivity.java
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/OfferActivity.java
@@ -56,11 +56,11 @@ public class OfferActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_offer);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_offer);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
 
 //        fab.setOnClickListener(new View.OnClickListener() {
