de132addcb14b5d386807e6da7c2b288a7e568e5
tim-hub <tim_w@outlook.com>
2018-06-19 15:09:54 +1200

    alter UI of offer

--- app/src/main/java/yuh/withfrds/com/hitchhiking/OfferActivity.java
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/OfferActivity.java
@@ -32,11 +32,6 @@ public class OfferActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
 
-
-
-
-
-
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
