a2735da8c1f9db9c7f9c2981f64a28dc0da0e109
tim-hub <tim_w@outlook.com>
2018-06-21 17:00:36 +1200

    alter encapsulate fire store to create our store

--- app/src/main/java/yuh/withfrds/com/hitchhiking/OfferActivity.java
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/OfferActivity.java
@@ -1,39 +1,16 @@
 package yuh.withfrds.com.hitchhiking;
 
 
-import android.net.Uri;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.SetOptions;
-import com.google.firebase.storage.FirebaseStorage;
 
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class OfferActivity extends BaseActivity {
@@ -45,13 +22,17 @@ public class OfferActivity extends BaseActivity {
     github.com/tim-hub
      */
 
-
+    public static String TIMEFORMAT = "HH:mm dd/MM/yyyy";
 
     private FirebaseFirestore db;
 
-    private TextView textStart;
-    private TextView textDest;
-    private TextView textPass;
+    private EditText textStart;
+    private EditText textDest;
+    private EditText textPass;
+
+    private EditText textTimeStart;
+    private EditText textTimeEnd;
+    private EditText textSeats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +48,8 @@ public class OfferActivity extends BaseActivity {
 //            }
 //        });
         initTexts();
+        initFirestore();
+
 
         Button offerButton = findViewById(R.id.buttonPost);
         offerButton.setOnClickListener(new View.OnClickListener() {
@@ -91,11 +74,15 @@ public class OfferActivity extends BaseActivity {
     }
 
     private void initTexts(){
-        initFirestore();
 
         textStart = (EditText) findViewById(R.id.textStartAddress);
         textDest = (EditText) findViewById(R.id.textDestAddress);
         textPass = (EditText) findViewById(R.id.textPassAddress);
+        textTimeStart = (EditText) findViewById(R.id.editTextDepartureStart);
+        textTimeEnd = findViewById(R.id.editTextDepartureEnd);
+        textSeats = findViewById(R.id.textSeats);
+
+
     }
 
 
@@ -104,37 +91,7 @@ public class OfferActivity extends BaseActivity {
     get fields and id of user
 
      */
-    private String getUserId(){
 
-        String uid ="";
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // User is signed in
-
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-
-            Uri photoUrl = user.getPhotoUrl();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
-            uid = user.getUid();
-
-
-
-        } else {
-            // No user is signed in
-        }
-
-        return uid;
-
-    }
 
     public void offer(View view){
         offer();
@@ -143,79 +100,26 @@ public class OfferActivity extends BaseActivity {
 
     private void offer(){
 
-        String uid = getUserId(); //we should get this id from auth
-
         String startPlace = textStart.getText().toString();
         String destination = textDest.getText().toString();
-
         String passPlaces = textPass.getText().toString();
 
+        int seats = Integer.parseInt(textSeats.getText().toString());
 
-        // get the mas which storage key and values
-        final Map<String, Object> offer = new HashMap<>();
+        Date timeStart = new Date();
+        Date timeEnd = new Date();
 
-        offer.put("from", startPlace);
-        offer.put("to", destination);
+        try {
 
+            timeStart = new SimpleDateFormat(TIMEFORMAT).parse(textTimeStart.getText().toString());
+            timeEnd = new SimpleDateFormat(TIMEFORMAT).parse(textTimeEnd.getText().toString());
 
-        final DocumentReference theUser= db.collection("Users").document(uid);
-
-        final CollectionReference offersCollection= theUser.collection("Offers");
-
-
-        theUser.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-
-                        offersCollection.add(offer);
+        }catch (Exception e){
+            Log.e("Error", "offer: " +e +"");
+        }
+        OurStore.postAnOffer(db, startPlace, destination, passPlaces, timeStart, timeEnd, seats);
 
 
-                    } else {
-                        Log.d("State", "No such document");
-
-                        Map<String, Object> userData = new HashMap<>();
-
-                        Map<String, Object> nestedData = new HashMap<>();
-                        // get more user profile from auth
-                        nestedData.put("userName", "username");
-
-                        userData.put("Info", nestedData);
-
-                        theUser.set(
-                            userData
-                        );
-
-                        CollectionReference offersCollection= theUser.collection("Offers");
-                        offersCollection.add(offer);
-
-                    }
-                } else {
-                    Log.d("Error", "get failed with ", task.getException());
-                }
-            }
-        });
-
-
-
-//        // add ducument
-//        db.collection("Users")
-//                .add(offer)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        Log.d("Success", "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                                          @Override
-//                                          public void onFailure(@NonNull Exception e) {
-//                                              Log.w("Error", "Error adding document", e);
-//                                          }
-//                }
-//                );
 
     }
 
--- app/src/main/java/yuh/withfrds/com/hitchhiking/OurStore.java
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/OurStore.java
@@ -1,7 +1,27 @@
 package yuh.withfrds.com.hitchhiking;
 
 
-/*
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class OurStore {
+    /*
+This is an encapsulation class for fire store
 
 如果后续时间充足，应该封装firestore 存储到这个类里，
 
@@ -17,9 +37,114 @@ package yuh.withfrds.com.hitchhiking;
 
  */
 
-public class OurStore {
 
 
+    /*
+
+    get fields and id of user
+
+     */
+    private static String getUserId(){
+
+        String uid ="";
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            uid = user.getUid();
+
+        }else{
+            Log.d("Error", "Firebase Auth error");
+        }
+        return uid;
+    }
+
+
+
+
+
+
+    public static void submitADocument(
+            FirebaseFirestore db,
+            String collectionName ,
+            String start, String dest,
+            String pass,
+            Date timeStart, Date timeEnd,
+            int seats
+    ){
+
+
+        String uid = getUserId(); //we should get this id from auth
+
+        String startPlace = start;
+        String destination = dest;
+        String passPlaces = pass;
+
+
+        // get the mas which storage key and values
+        final Map<String, Object> offer = new HashMap<>();
+
+        offer.put("from", startPlace);
+        offer.put("to", destination);
+
+
+        final DocumentReference theUser= db.collection("Users").document(uid);
+
+        final CollectionReference offersCollection= theUser.collection(collectionName);
+
+
+        theUser.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        offersCollection.add(offer);
+
+                    } else {
+                        Log.d("State", "No such document, the document will be created");
+
+                        Map<String, Object> userData = new HashMap<>();
+
+                        Map<String, Object> nestedData = new HashMap<>();
+                        // get more user profile from auth
+                        nestedData.put("userName", "username");
+
+                        userData.put("Info", nestedData);
+
+                        theUser.set(
+                                userData
+                        );
+
+                        // store the offer data
+
+                        offersCollection.add(offer);
+
+                    }
+                } else {
+                    Log.d("Error", "get failed with ", task.getException());
+                }
+            }
+        });
+
+    }
+
+
+    public static void postAnOffer(FirebaseFirestore db,
+                                   String start, String dest,
+                                   String pass,
+                                   Date timeStart, Date timeEnd,
+                                   int seats)
+    {
+        submitADocument(db, "Offers", start,dest,pass, timeStart, timeEnd, seats);
+    }
+
+    public static void postAnRequest(FirebaseFirestore db,
+                                     String start, String dest,
+                                     String pass,
+                                     Date timeStart, Date timeEnd,
+                                     int seats){
+        submitADocument(db, "Requests", start,dest,pass, timeStart, timeEnd, seats);
+    }
 
 
 }
