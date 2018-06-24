2f15775101e20e7345cadc769cad804cd10c08e6
tim-hub <tim_w@outlook.com>
2018-06-24 17:27:52 +1200

    remove map in side menu

--- app/src/main/java/yuh/withfrds/com/hitchhiking/BaseActivity.java
+++ app/src/main/java/yuh/withfrds/com/hitchhiking/BaseActivity.java
@@ -130,8 +130,8 @@ public class BaseActivity extends AppCompatActivity implements
                 startActivity(new Intent(this, RequestActivity.class));
                 return true;
 
-//            case R.id.action_search :
-//                startActivity(new Intent(this, SearchActivity.class));
+//            case R.id.action_matching :
+//                startActivity(new Intent(this, MatchingActivity.class));
 //                return true;
 
 
